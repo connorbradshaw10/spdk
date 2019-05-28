ee469160867b9939007e3e25a349228cf8d80601
Jan Kryl <jan.kryl@mayadata.io>
2019-05-28 16:01:46 +0000

    New construct_ms_lvol_bdev method allowing us to specify uuid of the new lvol
     Conflicts:
    	module/bdev/lvol/vbdev_lvol_rpc.c
    	scripts/rpc/lvol.py

--- module/bdev/lvol/vbdev_lvol.h
+++ module/bdev/lvol/vbdev_lvol.h
@@ -51,6 +51,10 @@ int vbdev_lvol_create(struct spdk_lvol_store *lvs, const char *name, uint64_t sz
 		      bool thin_provisioned, enum lvol_clear_method clear_method,
 		      spdk_lvol_op_with_handle_complete cb_fn,
 		      void *cb_arg);
+int vbdev_lvol_create_with_uuid(struct spdk_lvol_store *lvs, const char *name, uint64_t sz,
+				bool thin_provisioned, enum lvol_clear_method clear_method,
+				const char *uuid, spdk_lvol_op_with_handle_complete cb_fn,
+				void *cb_arg);
 
 int vbdev_lvol_create_with_opts(struct spdk_lvol_store *lvs,
 				const struct spdk_lvol_opts *lvol_opts,
