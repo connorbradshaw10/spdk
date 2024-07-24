96cb69f124da38584763ad3f48932db1612ea966
Ed Reed <edreed@microsoft.com>
2024-07-24 16:34:15 -0700

    test: fix reporting of unit test failures

--- .pipelines/templates/scripts/run-unit-tests.sh
+++ .pipelines/templates/scripts/run-unit-tests.sh
@@ -8,38 +8,33 @@ generate_results() {
     local total_duration
     local total_skipped
 
+    total_tests="${#status[@]}"
     total_failures=$(echo "${status[@]}" | jq -MR 'split(" ") | map(select(.=="failed")) | length')
     total_duration=$(echo "${duration[@]}" | jq -s 'add')
     total_skipped=$(echo "${status[@]}" | jq -MR 'split(" ") | map(select(.=="skipped")) | length')
 
+    declare -A testcase=()
+
+    for test in "${!status[@]}"; do
+        testcase[$test]=""
+        if [ "${status[$test]}" == "failed" ]; then
+            testcase[$test]+=$'\n'"            <failure message=\"\" type=\"failed\">${message[$test]}</failure>"
+        fi
+        if [ "${status[$test]}" != "skipped" ]; then
+            testcase[$test]+=$'\n'"            <system-out><![CDATA[[[ATTACHMENT|$test-test.log]]]]></system-out>"
+        fi
+        if [ "${testcase[$test]}" != "" ]; then
+            testcase[$test]+=$'\n        '
+        fi
+    done
+
     cat <<EOF > "${LoggingDirectory}/JUnit.xml"
 <?xml version="1.0" encoding="UTF-8"?>
-<testsuites tests="1" failures="${total_failures}" errors="0" time="0">
-    <testsuite name="SPDK Unit Tests" tests="${#status[@}]}" disabled="0" skipped="${total_skipped}" errors="0" failures="${total_failures}" time="${total_duration}">
-        <testcase name="SPDK Unit Test Setup" status="${status["setup"]}" time="${duration["setup"]}">
-            <system-out>
-                <![CDATA[
-${message["setup"]}
-[[ATTACHMENT|unit-test-setup.log]]
-]]>
-            </system-out>
-        </testcase>
-        <testcase name="SPDK Unit Tests" status="${status["unit"]}" time="${duration["unit"]}">
-            <system-out>
-                <![CDATA[
-${message["unit"]}
-[[ATTACHMENT|unit-tests.log]]
-]]>
-            </system-out>
-        </testcase>
-        <testcase name="SPDK Unit Test Cleanup" status="${status["cleanup"]}" time="${duration["cleanup"]}">
-            <system-out>
-                <![CDATA[
-${message["cleanup"]}
-[[ATTACHMENT|unit-test-cleanup.log]]
-]]>
-            </system-out>
-        </testcase>
+<testsuites name="SPDK Unit Tests" tests="${total_tests}" skipped="${total_skipped}" failures="${total_failures}" time="${total_duration}">
+    <testsuite name="SPDK Unit Tests" tests="${total_tests}" skipped="${total_skipped}" failures="${total_failures}" time="${total_duration}">
+        <testcase name="SPDK Unit Test Setup" status="${status["setup"]}" time="${duration["setup"]}">${testcase["setup"]}</testcase>
+        <testcase name="SPDK Unit Tests" status="${status["unit"]}" time="${duration["unit"]}">${testcase["unit"]}</testcase>
+        <testcase name="SPDK Unit Test Cleanup" status="${status["cleanup"]}" time="${duration["cleanup"]}">${testcase["cleanup"]}</testcase>
     </testsuite>
 </testsuites>
 EOF
@@ -91,22 +86,22 @@ declare -Agx message=(["setup"]="Setup skipped." ["unit"]="Unit tests skipped." 
 trap 'generate_results' ERR EXIT
 
 # Setup the environment for the unit tests.
-set +x
-command time --format="%e" --output="${LoggingDirectory}/unit-test-setup-time" "${SCRIPT_DIR}/unit-test/setup.sh" 2>&1 | tee "${LoggingDirectory}/unit-test-setup.log"
+set +e
+command time --quiet --format="%e" --output="${LoggingDirectory}/setup-test-time" "${SCRIPT_DIR}/unit-test/setup.sh" 2>&1 | tee "${LoggingDirectory}/setup-test.log"
 rc=$?
-set -x
+set -e
 if [ $rc -ne 0 ]; then
-    set_status "setup" "failed" "$(cat "${LoggingDirectory}/unit-test-setup-time")" "Setup failed."
+    set_status "setup" "failed" "$(cat "${LoggingDirectory}/setup-test-time")" "Setup failed."
     exit 1
 fi
 
-set_status "setup" "passed" "$(cat "${LoggingDirectory}/unit-test-setup-time")" "Setup succeeded."
+set_status "setup" "passed" "$(cat "${LoggingDirectory}/setup-test-time")" "Setup succeeded."
 
 # Run the unit tests.
-set +x
-command time --format="%e" --output="${LoggingDirectory}/unit-test-time" "${WorkingDirectory}/test/unit/unittest.sh" 2>&1 | tee "${LoggingDirectory}/unit-tests.log"
+set +e
+command time --quiet --format="%e" --output="${LoggingDirectory}/unit-test-time" "${WorkingDirectory}/test/unit/unittest.sh" 2>&1 | tee "${LoggingDirectory}/unit-test.log"
 rc=$?
-set -x
+set -e
 if [ $rc -ne 0 ]; then
     set_status "unit" "failed" "$(cat "${LoggingDirectory}/unit-test-time")" "Unit tests failed."
     exit 1
@@ -115,14 +110,14 @@ fi
 set_status "unit" "passed" "$(cat "${LoggingDirectory}/unit-test-time")" "Unit tests passed."
 
 # Cleanup the environment after the unit tests.
-set +x
-command time --format="%e" --output="${LoggingDirectory}/unit-test-cleanup-time" "${SCRIPT_DIR}/unit-test/cleanup.sh" 2>&1 | tee "${LoggingDirectory}/unit-test-cleanup.log"
+set +e
+command time --quiet --format="%e" --output="${LoggingDirectory}/cleanup-test-time" "${SCRIPT_DIR}/unit-test/cleanup.sh" 2>&1 | tee "${LoggingDirectory}/cleanup-test.log"
 rc=$?
-set -x
+set -e
 if [ $rc -ne 0 ]; then
-    set_status "cleanup" "failed" "$(cat "${LoggingDirectory}/unit-test-cleanup-time")" "Cleanup failed."
+    set_status "cleanup" "failed" "$(cat "${LoggingDirectory}/cleanup-test-time")" "Cleanup failed."
     exit 1
 fi
 
-set_status "cleanup" "passed" "$(cat "${LoggingDirectory}/unit-test-cleanup-time")" "Cleanup passed"
+set_status "cleanup" "passed" "$(cat "${LoggingDirectory}/cleanup-test-time")" "Cleanup passed"
 
